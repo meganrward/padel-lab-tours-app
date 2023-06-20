f6f6e316ea84217c419b79d8a3f9c4eb3006ec79
MeganAccenture <megan.ward@accenture.com>
2023-06-20 12:01:00 +0100

    notes app testing

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "padellabtoursapp": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "padellabtoursapp51ecf940": {
       "AppClientID": "string",
