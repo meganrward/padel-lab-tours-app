7b2e744604664e90ac56f8a5f5ea6ef5d366f850
MeganAccenture <megan.ward@accenture.com>
2023-07-10 00:14:45 +0100

    fixed sliders and locations page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,37 +1,62 @@
 import React from "react";
-import { View, Image, Flex, Tabs, TabItem } from "@aws-amplify/ui-react";
-import { Carousel } from 'react-responsive-carousel';
+import { View, Image, Text, Flex } from "@aws-amplify/ui-react";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, Autoplay } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
 import IMAGES from "./../helpers/images.js"
 
 export default function HomePage() {
     return (
         <>
-         <Flex direction="column" gap="2rem">
-    <Tabs spacing="equal">
-      <TabItem>            <Image
-      src="./la-reserva.jpg"
-      alt="la reserva"
-    /></TabItem>
-      <TabItem>
-      <Image
-      src={IMAGES[0]}
-      alt="la reserva"
-    />
-      </TabItem>
-      <TabItem>
-      <Image
-      src={IMAGES[1]}
-      alt="la reserva"
-    />
-      </TabItem>
-      <TabItem>
-      <Image
-      src={IMAGES[2]}
-      alt="la reserva"
-    />
-      </TabItem>
-    </Tabs>
-  </Flex>
+        <View direction="column" gap="2rem" height="100%">
+         <Swiper navigation={true} pagination={true} modules={[Pagination, Navigation, Autoplay]} className="mySwiper" autoplay={{ delay: 2500, disableOnInteraction: false }}>
+                <SwiperSlide>
+                    {/* <View 
+                        style= {{backgroundImage: "url(./la-reserva-3.jpg)"}} 
+                        objectFit="scale-down"
+                        height="100%"
+                        width="100%"
+                    >
+                        <View height="100%">hi</View>
+                    </View> */}
+                    <Image
+                        src="./la-reserva-3.jpg"
+                        alt="la reserva"
+                        objectFit="cover"
+                        height="50%"
+                        width="100%"
+                    />
+                    </SwiperSlide>
+                <SwiperSlide>
+                    <Image
+                        src={IMAGES[0]}
+                        alt="la reserva"
+                        objectFit="cover"
+                        height="50%"
+                        width="100%"
+                    />
+                </SwiperSlide>
+                <SwiperSlide>     
+                    <Image
+                        src={IMAGES[1]}
+                        alt="la reserva"
+                        objectFit="cover"
+                        height="50%"
+                        width="100%"
+                    />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <Image
+                        src={IMAGES[2]}
+                        alt="la reserva"
+                        objectFit="cover"
+                        height="50%"
+                        width="100%"
+                    />
+                </SwiperSlide>
+            </Swiper>
+  </View>
         </>
     )
 }
--- src/pages/LocationsPage.js
+++ src/pages/LocationsPage.js
@@ -1,58 +1,53 @@
 import React from "react";
 import { Image, Flex, Text, Heading, Card, View, Link, Tabs, TabItem } from '@aws-amplify/ui-react';
 import IMAGES from "./../helpers/images.js"
-
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, Autoplay } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
 
 export default function LocationsPage() {
   return (
     <>
-     <View
-      backgroundColor="white"
-      padding="medium"
-    >
-      <Card>
-        <Flex direction="row" alignItems="flex-start" justifyContent="space-between">
-          <Flex
+        <Flex
             direction="column"
             alignItems="flex-start"
             gap="10px"
             wrap="wrap"
-          >
-            <Heading level={5}>
-              SOTOGRANDE
-            </Heading>
+        >
+            <View textAlign="center" width="100%">
 
+            <Heading level={1} fontWeight="bold">
+                SOTOGRANDE
+            </Heading>
             <Text as="span">
                 La Reserva, Sotogrande
                 <Link href="https://www.lareservaclubsotogrande.com/" isExternal={true}>
-          <Text color="blue" paddingTop="26px">Visit website</Text></Link>
+                    <Text color="blue" paddingTop="26px">Visit website</Text>
+                </Link>
+                <Link href="/booking" isExternal={true}>
+                    <Text color="blue" paddingTop="26px">Book this tour</Text>
+                </Link>
             </Text>
-          </Flex>
-          <Flex width="80%">
-          <Tabs spacing="equal">
-      <TabItem>
-      <Image
-      src={IMAGES[3]}
-      alt="Arial view of Sotogrande artificial beach"
-      />
-      </TabItem>
-      <TabItem>
-      <Image
-      src={IMAGES[4]}
-      alt="View of tables and chairs on a lakeside beach, surrounded by palm tree"
-      />
-      </TabItem>
-      <TabItem>
-      <Image
-      src={IMAGES[5]}
-      alt="View of tables and chairs on a lakeside beach, surrounded by palm trees"
-      />
-      </TabItem>
-    </Tabs>
-    </Flex>
+            </View>
+            <Flex direction="row" minWidth="0" overflow="visible">
+                <Image
+                    src={IMAGES[3]}
+                    alt="Arial view of Sotogrande artificial beach"
+                    width="100%"
+                    />
+                <Image
+                    src={IMAGES[4]}
+                    alt="View of tables and chairs on a lakeside beach, surrounded by palm tree"
+                    width="100%"
+                    />  
+                <Image
+                    src={IMAGES[5]}
+                    alt="View of tables and chairs on a lakeside beach, surrounded by palm trees"
+                    width="100%"
+                    />
+            </Flex>
         </Flex>
-      </Card>
-    </View>
     </>
   );
 };
